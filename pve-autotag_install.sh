5f73b860ec5f54b823adf2175449114129e9f58c
ali2key <64281880+ali2key@users.noreply.github.com>
2024-12-05 11:34:04 +0100

    Add files via upload

---
+++ pve-autotag_install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -xe
+
+# install prerequisites
+apt install -y ipcalc
+
+# install lxc-iptag
+curl -sSL https://raw.githubusercontent.com/gitsang/lxc-iptag/main/lxc-iptag -o /usr/local/bin/lxc-iptag && chmod +x /usr/local/bin/lxc-iptag
+curl -sSL https://raw.githubusercontent.com/gitsang/lxc-iptag/main/lxc-iptag.conf -o /usr/local/etc/lxc-iptag.conf
+
+# configure lxc-iptag systemd
+curl -sSL https://raw.githubusercontent.com/gitsang/lxc-iptag/main/lxc-iptag.service -o /lib/systemd/system/lxc-iptag.service
+
+# start lxc-iptag
+systemctl daemon-reload
+systemctl enable lxc-iptag.service
+systemctl start lxc-iptag.service
